c59e2cbcc4b95f9dc686d0845c9368f3cfd0547c
jazema <jazema@student.42.fr>
2025-12-16 20:53:52 +0000

    Added nginx container

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,1 +1,17 @@
-nginx
+#!/bin/sh
+
+export WP_DIR="/var/www/html"
+
+export DB_NAME="$(cat /run/secrets/db_name)"
+export DB_USER="$(cat /run/secrets/db_user)"
+export DB_PASSWORD="$(cat /run/secrets/db_password)"
+
+if [ ! -f "$WP_DIR/wp-config.php" ]; then
+	echo "Wordpress config does not exit.. Creating"
+	#wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb --path="$WP_DIR"
+fi
+
+echo "Wordpress already installed: Starting..."
+
+#rm -f /var/www/html/xmlrpc.php
+tail -f
